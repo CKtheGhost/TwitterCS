52d6f41f91b73a5ba711e22393ebde735bc2c9e0
cktheghost <christiankearney3@gmail.com>
2025-05-03 09:49:14 -0400

    Add dummy consent.js to fix deployment

---
+++ backend/src/routes/consent.js
@@ -0,0 +1,8 @@
+/**
+ * Dummy Consent Routes - To be replaced by consent-simple.js
+ */
+const express = require('express');
+const router = express.Router();
+
+// Export empty router
+module.exports = router;
